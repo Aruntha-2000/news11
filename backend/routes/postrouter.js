2f3daf2556658f56620a9114dd51ec4e31ae50bc
Aruntha-2000 <aruntha@gmail.com>
2026-08-17 12:48:15 +0530

    fix authmiddleware filename

--- backend/routes/postrouter.js
+++ backend/routes/postrouter.js
@@ -4,7 +4,7 @@ const {
   createPost
 } = require("../controllers/postcontroller");
 
-const authenticateToken = require("../middleware/authMiddleware");
+const authenticateToken = require("../middleware/authmiddleware");
 
 const router = express.Router();
 
