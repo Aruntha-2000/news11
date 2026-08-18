ccfa26d0920bf0df716ff17ac19d6a0ce9c16eb1
Aruntha-2000 <aruntha@gmail.com>
2026-08-18 14:02:13 +0530

    Fix Railway MySQL connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: Number(process.env.MYSQLPORT)
-});
+let db;
+
+if (process.env.MYSQL_URL) {
+  // Railway
+  db = mysql.createPool(process.env.MYSQL_URL);
+} else {
+  // Local XAMPP
+  db = mysql.createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: Number(process.env.MYSQLPORT)
+  });
+}
 
 db.getConnection((err, connection) => {
   if (err) {
