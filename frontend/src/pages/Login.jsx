144eb6a810254dd505c4ab1f27894c63b32083bf
Aruntha-2000 <arunthasujenthan@gmail.com>
2026-08-21 17:52:27 +0530

    update frontend and backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -274,7 +274,7 @@ function Login() {
               </>
             ) : (
               <>
-                🔐 Login
+                 Login
               </>
             )}
           </button>
