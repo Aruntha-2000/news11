39547e6585364397727ccea9d9725f66913de75d
Aruntha-2000 <arunthasujenthan@gmail.com>
2026-08-21 19:40:59 +0530

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -80,8 +80,7 @@ function App() {
       <main className="page-container">
         <Routes>
           {/* HOME & AUTH */}
-          <Route path="/news" element={<NewsFeed />} />
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<NewsFeed />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
 
