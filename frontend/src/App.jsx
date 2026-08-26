0ad8c24b763750ba10c23dd1e540c9c6b7d439ca
Aruntha-2000 <arunthasujenthan@gmail.com>
2026-08-26 16:43:33 +0530

    update frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 // =====================================================
 
 import Login from "./pages/Login";
-import Register from "./pages/newsfeed";
+import Register from "./pages/Register";
 import NewsFeed from "./pages/newsfeed";
 import NewsDetails from "./pages/newdetails";
 import Profile from "./pages/profile";
