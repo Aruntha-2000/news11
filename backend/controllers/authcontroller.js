3351488d282f754902927bc44bb5ca0055b53493
Aruntha-2000 <arunthasujenthan@gmail.com>
2026-08-29 15:06:06 +0530

    update  backend and frontend

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -85,20 +85,19 @@ const register = async (req, res) => {
     const verificationLink =
       `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`;
 
-          const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        },
-        family: 4,
-        connectionTimeout: 30000,
-        greetingTimeout: 30000,
-        socketTimeout: 30000
-      });
+     const transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+          },
+          family: 4,
+          connectionTimeout: 30000,
+          greetingTimeout: 30000,
+          socketTimeout: 30000
+        });
 
     // Send verification email
     await transporter.sendMail({
